cf5b8c1aaed23bc3564fab5b5ef570abe24102b0
zacharyb12 <boualemzachary89@yahoo.fr>
2026-09-08 12:29:34 +0200

    demo formulaire ajout movie

--- cinestorm/src/app/features/movie/add-movie/add-movie.component.ts
+++ cinestorm/src/app/features/movie/add-movie/add-movie.component.ts
@@ -1,13 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { IonContent, IonHeader, IonBackButton } from '@ionic/angular';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, ɵInternalFormsSharedModule, ReactiveFormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonBackButton, NavController, IonCard, IonInput, IonLabel, IonTextarea, IonButton } from '@ionic/angular';
+import { MovieService } from '../../../core/movie-service/movie-service';
+import { CreateMovie } from '../movies-model';
 
 @Component({
   selector: 'app-add-movie',
   templateUrl: './add-movie.component.html',
   styleUrls: ['./add-movie.component.scss'],
-  imports: [IonHeader, IonContent, IonBackButton],
+  // nécéssite un import
+  imports: [IonHeader, IonContent, IonBackButton, IonCard, IonInput, IonLabel, IonTextarea, ReactiveFormsModule, IonButton],
 })
+
 export class AddMovieComponent {
 
+  // construire le formulaire
+formMovie = new FormGroup({
+  title : new FormControl('',[Validators.required,Validators.minLength(3)]),
+  description : new FormControl('',[Validators.required,Validators.maxLength(100)]),
+  year : new FormControl(0,[Validators.required,Validators.min(1950),Validators.max(2030)]),
+  imageUrl : new FormControl('')
+})
 
+// injection du service movie
+  private movieService = inject(MovieService)
+
+  // injection du nav controller
+  private nav = inject(NavController)
+
+  // methode pour la soumission du formulaire
+  onSubmit(){
+    if(this.formMovie.valid){
+      const valueForm = this.formMovie.value
+      
+      const movieToAdd : CreateMovie = this.formMovie.value as CreateMovie
+      
+      
+      this.movieService.addMovie(movieToAdd)
+
+      this.nav.navigateForward(['/movies'])
+    }
+    
+  }
 }
